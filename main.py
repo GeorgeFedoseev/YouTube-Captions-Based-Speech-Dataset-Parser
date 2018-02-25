386c50a17fcbd40cbdb1eb022b5a400088cfd488
George Fedoseev <george.fedoseev@me.com>
2018-02-25 23:26:21 +0300

    parsing subs

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import pafy
 from subprocess import call
 
-video_url = "https://www.youtube.com/watch?v=j6TL7ufqoLc"
+import pyvtt
+
+'''
+video_url = "https://www.youtube.com/watch?v=ypEPe5Ii3Aw"
 
 video = pafy.new(video_url)
 print video.title
 
-'''
+
 for stream in video.audiostreams:
 	print stream
 
 video.audiostreams[0].download()
+
+
+call(["youtube-dl", "--write-sub", "--sub-lang", "ru",
+ "--skip-download",
+ "-o", "./subs.vtt",
+  video_url])
 '''
 
-call(["youtube-dl", "--write-sub", "--sub-lang", "ru", "--skip-download", video_url])
+subs_path = "./subs.ru.vtt"
+
+
+
+
+subs = pyvtt.WebVTTFile.open(subs_path)
+
+sub = subs[0]
+print sub.start.ordinal
