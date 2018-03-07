cf05733e4f6c7abff8158cc49f047aa5fe470474
George Fedoseev <george.fedoseev@me.com>
2018-03-07 19:39:56 +0300

    better clearing text

--- subs_utils.py
+++ subs_utils.py
@@ -3,15 +3,32 @@
 
 
 import re
+import cyrtranslit
+from num2words import num2words
 
+
+def cvt_num(num_str):
+    return num2words(int(num_str), lang='ru')
 
 def clear_subtitle_text(text):
     cleared_text = text
+
+
+    cleared_text = cyrtranslit.to_cyrillic(cleared_text, 'ru')
+
+    # convert latin
+    cleared_text = cyrtranslit.to_cyrillic(cleared_text, 'ru')
+
+    #convert numbers 
+    cleared_text = re.sub(r'\d+', lambda m: cvt_num(m.group()), cleared_text)
+
 
     cleared_text = cleared_text.replace("\n", " ")
     cleared_text = re.sub(u'[^a-zA-Zа-яА-Я ]+', '', cleared_text, re.UNICODE)
     cleared_text = cleared_text.strip()
     cleared_text = re.sub(' +', ' ', cleared_text)
     cleared_text = cleared_text.lower()
+
+
 
     return cleared_text
--- test.py
+++ test.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-from vad import VoiceActivityDetector
+import cyrtranslit
+from num2words import num2words
+import re
 
-from subprocess import call
-
-wav_path = "/Users/gosha/Projects/youtube-audio-captions-parsing/data/blSz672ZarU/parts/blSz672ZarU-50160-56480.wav"
-#wav_path = '/Users/gosha/Projects/youtube-audio-captions-parsing/data/h37C0iPGBRU/parts/h37C0iPGBRU-5530-7620.wav'
-output_fragment_path = '/Users/gosha/Desktop/test.wav'
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
-v = VoiceActivityDetector(wav_path)
-raw_detection = v.detect_speech()
-speech_intervals = v.convert_windows_to_readible_labels(raw_detection)
-print speech_intervals
-print speech_intervals[0]['speech_begin']
+def cvt_num(num_str):
+    return num2words(int(num_str), lang='ru')
 
-speech_start = speech_intervals[0]['speech_begin']-0.2
-speech_end = speech_intervals[-1]['speech_end']+0.2
+s = '1 perspective операционка 2'
 
-print 'cut '+str(speech_start)+'-'+str(speech_end)
+print cyrtranslit.to_cyrillic(s, 'ru')
 
-# cut silence
-call(["ffmpeg", "-y",
-     "-i", wav_path,
-     "-ss", str(speech_start),
-     "-to", str(speech_end),
-     "-ac", "1",
-     "-ab", "16",
-     "-ar", "16000",         
-     output_fragment_path
-     ])
+print re.sub(r'\d+', lambda m: cvt_num(m.group()), s)
+
