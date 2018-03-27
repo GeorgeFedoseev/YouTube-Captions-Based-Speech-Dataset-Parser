701d280d0c4423489aa042a34bb460b0b9b14bd1
George Fedoseev <george.fedoseev@me.com>
2018-03-27 23:43:48 +0300

    remove extra data script

---
+++ remove_extra_data_for_processed.py
@@ -0,0 +1,54 @@
+import os
+import csv
+
+import const
+
+import sys
+
+import shutil
+
+import subprocess
+
+from glob import glob
+
+def remove_extra_data():
+    curr_dir_path = os.path.dirname(os.path.realpath(__file__))
+    videos_data_dir = os.path.join(curr_dir_path, "data/")
+
+    total_scanned = 0
+    removed = 0
+
+    for item in os.listdir(videos_data_dir):
+        item_path = os.path.join(videos_data_dir, item)
+
+        if not os.path.isdir(item_path):
+            continue        
+
+        total_scanned += 1
+
+        stats_path = os.path.join(item_path, "stats.csv")
+
+        # remove extra files if found stats file        
+        
+        if os.path.exists(stats_path):
+            for path in glob(os.path.join(item_path, "*.lock")):
+                os.remove(path)
+                removed += 1
+            for path in glob(os.path.join(item_path, "audio.*")):
+                os.remove(path)
+                removed += 1
+            for path in glob(os.path.join(item_path, "*.vtt")):
+                os.remove(path)
+                removed += 1
+
+            
+            
+            
+
+    
+    print 'Removed %i extra files folders from %i total' % (removed, total_scanned)
+
+
+if __name__ == '__main__':
+    remove_extra_data()
+        
