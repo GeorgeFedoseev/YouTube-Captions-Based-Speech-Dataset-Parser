7615e5b9afc30aae66cd2a3c8b2f473ea4162530
George Fedoseev <george.fedoseev@me.com>
2018-04-13 01:33:43 +0300

    increase accuracy of VAD by 2

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -104,7 +104,7 @@ def cut_audio_piece_to_wav(in_audio_path, out_audio_path, start_sec, end_sec):
 
 # WAVE CUT CORRECTION
 SPEECH_FRAME_SEC = 0.01
-CHECK_FRAMES_NUM = 5
+CHECK_FRAMES_NUM = 6
 
 def get_speech_int_array(wave, start, end):
     vad = webrtcvad.Vad(3)
